57f45d3b523720e138ef2bf88d2d0b1e9b45b5d5
rafael-lassance <rafael.loamartinez@gmail.com>
2020-09-18 20:36:41 -0300

    Segundo commit do projeto Java

---
+++ src/excecoes/TestException2.java
@@ -0,0 +1,36 @@
+package excecoes;
+
+public class TestException2 {
+	public static void main(String[] args) {
+		System.out.println("Inicio do metodo main");
+		metodo1();
+		System.out.println("Fim do metodo main");
+	}
+	
+	private static void metodo1() {
+		System.out.println("Inicio do metodo1");
+		metodo2();
+		System.out.println("Fim do metodo1");
+	}
+
+	private static void metodo2() {
+		System.out.println("Inicio do metodo2");
+		int array[];
+		array = new int [10];
+		
+		
+		try {
+			for (int i = 0; i < 20; i++) {
+				array[i]=i;
+				System.out.println(i);
+			}
+		}
+		catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("excecao pega (catched): " + e);
+		}
+		
+		
+		System.out.println("Fim do metodo2");
+		
+	}
+}
