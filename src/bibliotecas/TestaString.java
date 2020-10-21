3fa9fabdba60525012b2e83fa0a42614d5924814
rafael-lassance <rafael.loamartinez@gmail.com>
2020-10-21 11:42:23 -0300

    Explicando métodos da classe String na classe TestaString

--- src/bibliotecas/TestaString.java
+++ src/bibliotecas/TestaString.java
@@ -70,6 +70,98 @@ public class TestaString {
 		 **************************************************/
 	
 		 
+		/*********************
+		 * 
+		 * charAt:
+		 * retorna o valor char no índice especificado.
+		 * 
+		 *********************/
+		char f = a.charAt(0);
+		System.out.println("Valor char no indice 0: " + f);
+		
+		
+		
+		/*********************
+		 * 
+		 * length:
+		 * retorna o tamanho desta String.
+		 * 
+		 *********************/
+		int g = a.length();
+		System.out.println("Tamanho da String: " + g);
+		
+		
+		/*********************
+		 * 
+		 * substring:
+		 * retorna uma String que é uma substring desta String.
+		 * 
+		 *********************/
+		String h = a.substring(5);
+		System.out.println("Substring a partir do indice 5: " + h);
+		
+		String m = a.substring(5,10);
+		System.out.println("Substring a partir do indice 5 ate o indice 10: " + m);
+		
+		
+		
+		/*********************
+		 * 
+		 * indexOf:
+		 * retorna o índice dentro da String da primeira ocorrência do char especificado.
+		 * 
+		 *********************/
+		int n = a.indexOf("a");
+		System.out.println("Indice do char 'a': " + n);
+		
+		
+		/*********************
+		 * 
+		 * lastIndexOf:
+		 * retorna o índice dentro da String da última ocorrência do char especificado.
+		 * 
+		 *********************/
+		int o = a.lastIndexOf("a");
+		System.out.println("Ultimo indice do char 'a': " + o);
+		
+		
+		/*********************
+		 * 
+		 * isEmpty:
+		 * retorna true se, e somente se, o tamanho da String é 0.
+		 * 
+		 *********************/
+		boolean p = a.isEmpty();
+		System.out.println("String vazia?: " + p);
+		
+		/*********************
+		 * 
+		 * contains:
+		 * retorna true se, e somente se, esta String contém a sequência especificada de valores char.
+		 * 
+		 *********************/
+		boolean r = a.contains("Java");
+		System.out.println("String contém a sequência de char 'Java' ? : " + r);
+		
+		
+		/*********************
+		 * 
+		 * replace:
+		 * retorna uma String resultante da substituição de todas as ocorrências do charAntigo pelo charNovo.
+		 * 
+		 *********************/
+		String s = a.replace('a', 'e');
+		System.out.println("Substituir o char 'a' por 'e': " + s);
+		
+		/*********************
+		 * 
+		 * replace:
+		 * Substitui cada substring desta String igual à sequência alvo pela sequência de substituição.
+		 * 
+		 *********************/
+		String t = a.replace("Java", "C++");
+		System.out.println("Substituir a sequência 'Java' por 'C++' : " + t);
+		
 		
 		
 
